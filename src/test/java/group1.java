11803efa9af3e8a8387e380eb06a41aeec0324b4
belovasv <belovasv@gmail.com>
2020-12-21 09:00:01 +0300

    initial

---
+++ src/test/java/group1.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class group1 {
+
+    @Test
+    void testOne() {
+        assertTrue(true);
+    }
+
+    @Test
+    void testTwo() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void testThree() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled ("need time")
+    void testFour() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void testFive() {
+        assertTrue(true);
+    }
+}
